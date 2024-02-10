b95a6b01ead2719d9a79798efd66ee4f533d11e5
CyberQueen11 <alvlac0326@skola.goteborg.se>
2024-02-10 17:11:14 +0100

    Edited class file Main. Created tortoise instance and then printed out info

--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -2,10 +2,25 @@ package se.yrgo;
 
 import se.yrgo.animals.*;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        //Insert created animal instances here
+        List<Animal> animals = new ArrayList<>();
+
         // TODO create instances of animals
+        Tortoise galapagosTortoise = new Tortoise(
+                "Galapagos tortoise James", 68, 112.65);
+        galapagosTortoise.makeSound();
+        animals.add(galapagosTortoise);
+
         // TODO print information about all our animals
+        for (Animal animal : animals){
+            System.out.println(animal);
+            animal.makeSound();
+        }
     }
 }
