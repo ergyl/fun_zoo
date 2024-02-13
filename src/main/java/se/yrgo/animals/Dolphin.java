569a80c8a90dd9a8c58fedff4d0ee5840614e523
LudwigWagberg <ludwag0809@skola.goteborg.se>
2024-02-13 16:13:07 +0100

    Fixing the logic in dolphin class

--- src/main/java/se/yrgo/animals/Dolphin.java
+++ src/main/java/se/yrgo/animals/Dolphin.java
@@ -12,22 +12,19 @@ public class Dolphin extends Animal {
     }
 
     public String dolphinEats() {
-        if (getAge() > 1 || getAge() <= 10) {
+        if (getAge() > 1 && getAge() <= 10) {
             return "squid";
-        }
-        if (getAge() > 10 || getAge() <= 30) {
+        } else if (getAge() > 10 && getAge() <= 30) {
             return "shrimps";
-        }
-        if (getAge() > 30 || getAge() <= 50) {
+        } else if (getAge() > 30 && getAge() <= 50) {
             return "jellyfish";
-        }
-        if (getAge() > 50 || getAge() <= 70) {
+        } else if (getAge() > 50 && getAge() <= 70) {
             return "octopuses";
-        }
-        if (getAge() > 1 || getAge() <= 100) {
+        } else if (getAge() > 70 && getAge() <= 100) {
             return "crab";
+        } else {
+            return "fish";
         }
-        return "fish";
     }
 
     @Override
--- src/test/java/se/yrgo/animals/DolphinTest.java
+++ src/test/java/se/yrgo/animals/DolphinTest.java
@@ -2,29 +2,32 @@ package se.yrgo.animals;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 import se.yrgo.animals.Dolphin;
 import se.yrgo.utils.Gender;
 
 public class DolphinTest {
 
+    @Test
     public void testdolphinEats () {
         Dolphin age9 = new Dolphin("philips", 9, Gender.MALE, 200);
-        assertEquals(age9, age9.dolphinEats());
+        assertEquals("squid", age9.dolphinEats());
 
         Dolphin age20 = new Dolphin("mia", 20, Gender.FEMALE, 459);
-        assertEquals(age20, age20.dolphinEats());
+        assertEquals("shrimps", age20.dolphinEats());
 
         Dolphin age41 = new Dolphin("stones", 41, Gender.MALE, 459);
-        assertEquals(age41, age41.dolphinEats());
+        assertEquals("jellyfish", age41.dolphinEats());
 
         Dolphin age62 = new Dolphin("zidane", 62, Gender.MALE, 459);
-        assertEquals(age62, age62.dolphinEats());
+        assertEquals("octopuses", age62.dolphinEats());
 
         Dolphin age99 = new Dolphin("rashford", 99, Gender.MALE, 459);
-        assertEquals(age99, age99.dolphinEats());
+        assertEquals("crab", age99.dolphinEats());
 
         Dolphin age329 = new Dolphin("carola", 329, Gender.FEMALE, 459);
-        assertEquals(age329, age329.dolphinEats());
+        assertEquals("fish", age329.dolphinEats());
 
     }
 }
