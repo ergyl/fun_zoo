d72cb3a3160634078d0e798313ea4f7159c26f29
Erik Gylleus <erik.gylleus@outlook.com>
2024-02-14 00:08:24 +0100

    added javadoc to Elephant methods

--- src/main/java/se/yrgo/animals/Elephant.java
+++ src/main/java/se/yrgo/animals/Elephant.java
@@ -11,11 +11,21 @@ public class Elephant extends Animal {
         super(name, age, gender, weight);
     }
 
+    /**
+     * The unique sound of the elephant
+     * @return sound
+     */
     @Override
     public String makeSound() {
         return "tooot toooooot!";
     }
 
+    /**
+     * Method used to determine the elephant's memory capacity
+     * which is determined by its age. Older elephants have better memory.
+     * @return memory capacity of the elephant
+     * @throws IllegalArgumentException if its age is less than 0.
+     */
     public int getMemoryCapacity() {
         if (this.getAge() < 0) {
             throw new IllegalArgumentException("Age cannot be less than 0");
