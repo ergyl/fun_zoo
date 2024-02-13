1fd26fb0a77dee82f24781edb63fb7548915fc7b
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-02-13 16:40:56 +0100

    removed comments from Main

--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -5,33 +5,29 @@ import se.yrgo.utils.Gender;
 import se.yrgo.utils.ZooAction;
 import se.yrgo.zoo.Zoo;
 
-import java.util.*;
-
+/**
+ * The entry point for the application.
+ */
 public class Main {
 
     public static void main(String[] args) {
 
-        //Insert created animal instances here
-        List<Animal> animals = new ArrayList<>();
-
-        // TODO create instances of animals
         Tortoise galapagosTortoise = new Tortoise("James", 68, Gender.FEMALE, 123.24);
-        galapagosTortoise.makeSound();
-        animals.add(galapagosTortoise);
-
-
-        Elephant ben = new Elephant("Ben", 25, Gender.MALE, 6191.5);
         Tortoise rebecca = new Tortoise("Rebecca", 40, Gender.FEMALE, 104.4);
         Tortoise jim = new Tortoise("Jim", 34, Gender.MALE, 109.5);
+
+        Elephant ben = new Elephant("Ben", 25, Gender.MALE, 6191.5);
         Elephant bob = new Elephant("Bob", 10, Gender.MALE, 5999.9);
         Elephant rico = new Elephant("Rico", 19, Gender.MALE, 5895.1);
         Elephant sheila = new Elephant("Sheila", 11, Gender.FEMALE, 5559.9);
         Elephant pedro = new Elephant("Stefan", 40, Gender.MALE, 6019.6);
         Elephant maude = new Elephant("Maude", 55, Gender.FEMALE, 6193.4);
+
         Dolphin dolphin = new Dolphin("hera", 12, Gender.MALE, 1404);
+
         Tiger scar = new Tiger("Scar", 40, Gender.MALE, 50);
         Tiger simba = new Tiger("Simba", 5, Gender.MALE, 10);
-        //Create the zoo
+
         Zoo theZoo = new Zoo("Paradise Zoo", "Lincoln Avenue 199", "1920 KO", "USA");
 
         ZooAction.addAnimal(theZoo, galapagosTortoise);
