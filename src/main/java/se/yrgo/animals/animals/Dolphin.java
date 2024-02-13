44afb11e818a908d338648b21e490390cefadb6f
LudwigWagberg <ludwag0809@skola.goteborg.se>
2024-02-13 15:40:58 +0100

    Adding dolphin test and another method

--- src/main/java/se/yrgo/animals/animals/Dolphin.java
+++ src/main/java/se/yrgo/animals/animals/Dolphin.java
@@ -11,6 +11,25 @@ public class Dolphin extends Animal {
         super(name, age, gender, weight);
     }
 
+    public String dolphinEats() {
+        if (getAge() > 1 || getAge() <= 10) {
+            return "squid";
+        }
+        if (getAge() > 10 || getAge() <= 30) {
+            return "shrimps";
+        }
+        if (getAge() > 30 || getAge() <= 50) {
+            return "jellyfish";
+        }
+        if (getAge() > 50 || getAge() <= 70) {
+            return "octopuses";
+        }
+        if (getAge() > 1 || getAge() <= 100) {
+            return "crab";
+        }
+        return "fish";
+    }
+
     @Override
     public String makeSound() {
         return "Clicks whistles";
--- src/test/java/se/yrgo/animals/DolphinTest.java
+++ src/test/java/se/yrgo/animals/DolphinTest.java
@@ -1,4 +1,30 @@
 package se.yrgo.animals;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import se.yrgo.animals.animals.Dolphin;
+import se.yrgo.animals.utils.Gender;
+
 public class DolphinTest {
+
+    public void testdolphinEats () {
+        Dolphin age9 = new Dolphin("hernandinhio", 9, Gender.MALE, 200);
+        assertEquals(age9, age9.dolphinEats());
+
+        Dolphin age20 = new Dolphin("mia", 20, Gender.FEMALE, 459);
+        assertEquals(age20, age20.dolphinEats());
+
+        Dolphin age41 = new Dolphin("mia", 20, Gender.FEMALE, 459);
+        assertEquals(age41, age41.dolphinEats());
+
+        Dolphin age62 = new Dolphin("mia", 20, Gender.FEMALE, 459);
+        assertEquals(age62, age62.dolphinEats());
+
+        Dolphin age99 = new Dolphin("mia", 20, Gender.FEMALE, 459);
+        assertEquals(age99, age99.dolphinEats());
+
+        Dolphin age329 = new Dolphin("mia", 20, Gender.FEMALE, 459);
+        assertEquals(age329, age329.dolphinEats());
+
+    }
 }
